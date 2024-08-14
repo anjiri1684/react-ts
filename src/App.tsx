a8babb64f0f1679f74c200df01f753440a38a904
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-14 16:25:40 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
   return (
-    <div>
+    <TodosContextProvider>
       <NewTodo />
       <Todos />
-    </div>
+    </TodosContextProvider>
   );
 }
 
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,7 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useContext } from "react";
+import { TodosContext } from "../store/todos-context";
 import classes from "./NewTodo.module.css";
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -13,7 +15,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
       return;
     }
 
-    props.onAddTodo(enteredText);
+    todosCtx.addTodo(enteredText);
   };
   return (
     <form onSubmit={submitHandler} className={classes.form}>
